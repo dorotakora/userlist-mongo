fdd7d4dbe76ffbd3575303101a7afb24c66b147c
dorotakora <dorota.kora@o2.pl>
2019-07-24 17:02:27 +0200

    add new features

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,6 +1,7 @@
 
 // Userlist data array for filling in info box
 let userListData = [];
+let thisUserObject;
 
 // DOM Ready =============================================================
 $(document).ready(function() {
@@ -11,7 +12,10 @@ $(document).ready(function() {
     // Username link click
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 
-    // Open and close user form
+    // Hide modal
+    $('.close').on('click', hideModal);
+
+        // Open and close user form
     $('button.open-button').on('click', openForm);
     $('.cancel').on('click', closeForm);
 
@@ -23,7 +27,7 @@ $(document).ready(function() {
 
     // Update User link click
     $('#userList table tbody').on('click', 'td a.linkedituser', editUser);
-    $('#userList table tbody').on('click','td a.linksubmituser', submitUser);
+    $('#btnSubmitUser').on('click', submitUser);
 
 });
 
@@ -63,6 +67,9 @@ function showUserInfo(event) {
     // Prevent Link from Firing
     event.preventDefault();
 
+    // Show modal
+    $('.modal').css("display", "block");
+
     // Retrieve username from link rel attribute
     var thisUserName = $(this).attr('rel');
 
@@ -80,11 +87,17 @@ function showUserInfo(event) {
 
 };
 
+//Hide modal
+
+function hideModal() {
+    $('.modal').css("display", "none");
+}
+
 // Add User
 function addUser(event) {
     event.preventDefault();
 
-    // Super basic validation - increase errorCount variable if any fields are blank
+    // Basic validation - increase errorCount variable if any fields are blank
     let errorCount = 0;
     $('#addUser input').each(function(index, val) {
         if($(this).val() === '') { errorCount++; }
@@ -134,6 +147,9 @@ function addUser(event) {
         alert('Please fill in all fields');
         return false;
     }
+
+    //Hide popup after new user is added
+    $('.form-popup').css('display', 'none');
 };
 
 // Delete User
@@ -181,10 +197,18 @@ function editUser(event) {
     // Prevent Link from Firing
     event.preventDefault();
 
+    console.log(this);
+    //Show popup
+    $('.form-popup').css('display', 'flex');
+
+    //Show button submit user
+    $('#btnSubmitUser').css('display', 'flex');
+
     // Change AddUser table to EditUser table
     $('#addUserTitle').html('Edit User');
     $('#btnAddUser').css("display", "none");
-    $(this, '.linkedituser').html('submit').toggleClass('linkedituser linksubmituser');
+    // $(this, '.linkedituser').html('submit').toggleClass('linkedituser linksubmituser');
+    $(this, '.linkedituser').html('submit');
 
     // Retrieve username from link rel attribute
     var thisUserId = $(this).attr('rel');
@@ -193,7 +217,7 @@ function editUser(event) {
     var arrayPosition = userListData.map(function(arrayItem) { return arrayItem._id; }).indexOf(thisUserId);
 
     // Get our User Object
-    var thisUserObject = userListData[arrayPosition];
+    thisUserObject = userListData[arrayPosition];
 
     //Populate Info Box
         $('#addUser fieldset input#inputUserName').val(thisUserObject.username);
@@ -207,8 +231,10 @@ function editUser(event) {
 // Edit User
 function submitUser(event) {
     event.preventDefault();
+
     $('#addUserTitle').html('Add User');
-    $('#btnAddUser').css("display", "block");
+    $('#btnAddUser').css("display", "flex");
+    $('#btnSubmitUser').css("display", "none");
 
     // Super basic validation - increase errorCount variable if any fields are blank
     var errorCount = 0;
@@ -233,7 +259,7 @@ function submitUser(event) {
         // If they did, do our edit
         $.ajax({
             type: 'PUT',
-            url: '/users/edituser/' + $(this).attr('rel'),
+            url: '/users/edituser/' + thisUserObject._id,
             data: newEditUser
         }).done(function( response ) {
 
@@ -260,17 +286,30 @@ function submitUser(event) {
         alert('Please fill in all fields');
         return false;
     }
+    //Hide popup
+    $(".form-popup").css("display", "none");
 };
 
 //Open Form
-function openForm(event) {
-    event.preventDefault();
-    console.log('cośśśśś');
-  $("#myForm").css("display", "block");
+function openForm() {
+    $(".form-popup").css("display", "block");
 }
-//Open Form
-function closeForm(event) {
-    event.preventDefault();
-
+//Close Form
+function closeForm() {
+    //Show button addUser
+    $("#btnAddUser").css("display", "flex");
+    //Hide popup
+    $("#addUserTitle").html("Add user");
+    //Hide popup
     $(".form-popup").css("display", "none");
+    //Hide submit button
+    $("#btnSubmitUser").css("display", "none");
+    // Clear the form inputs
+    $('#addUser fieldset input').val('');
+    //Change linkedituser html
+    if ($('.linkedituser').html() === 'submit') {
+        $('.linkedituser').addClass('fa fa-lg fas fa-edit')
+    }
+    // $('.linkedituser').html('submit').removeClass('linksubmituser').addClass('linkedituser');
+    $('.linkedituser').html('submit').html('<i class="fa fa-lg fas fa-edit">');
 }
